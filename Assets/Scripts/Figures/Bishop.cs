3a2131911a790a6a476c9781a6b77b92bc9bf3dc
MarcelJurtz <jurtzmarcel@gmail.com>
2017-11-03 16:33:49 +0100

    Add moveset for bishop

--- Assets/Scripts/Figures/Bishop.cs
+++ Assets/Scripts/Figures/Bishop.cs
@@ -4,5 +4,81 @@ using UnityEngine;
 
 public class Bishop : ChessFigure
 {
+    public override bool[,] PossibleMove()
+    {
+        bool[,] r = new bool[8, 8];
 
+        ChessFigure c;
+        int i, j;
+
+        // Top Left
+        i = CurrentX;
+        j = CurrentY;
+        while(true)
+        {
+            i--;
+            j++;
+            if (i < 0 || j >= 8) break;
+            c = BoardManager.Instance.ChessFigurePositions[i, j];
+            if (c == null) r[i, j] = true;
+            else
+            {
+                if (c.isWhite != isWhite) r[i, j] = true;
+                break;
+            }
+        }
+
+        // Top Right
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i++;
+            j++;
+            if (i >= 8 || j >= 8) break;
+            c = BoardManager.Instance.ChessFigurePositions[i, j];
+            if (c == null) r[i, j] = true;
+            else
+            {
+                if (c.isWhite != isWhite) r[i, j] = true;
+                break;
+            }
+        }
+
+        // Bottom Left
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i--;
+            j--;
+            if (i < 0 || j < 0) break;
+            c = BoardManager.Instance.ChessFigurePositions[i, j];
+            if (c == null) r[i, j] = true;
+            else
+            {
+                if (c.isWhite != isWhite) r[i, j] = true;
+                break;
+            }
+        }
+
+        // Bottom Right
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i++;
+            j--;
+            if (i >= 8 || j < 0) break;
+            c = BoardManager.Instance.ChessFigurePositions[i, j];
+            if (c == null) r[i, j] = true;
+            else
+            {
+                if (c.isWhite != isWhite) r[i, j] = true;
+                break;
+            }
+        }
+
+        return r;
+    }
 }
