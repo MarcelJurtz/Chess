be51c9f61bf0f8358ac5eab359f648c0c23b7fa8
MarcelJurtz <jurtzmarcel@gmail.com>
2017-11-03 16:21:26 +0100

    Add moveset for rook

--- Assets/Scripts/Figures/Rook.cs
+++ Assets/Scripts/Figures/Rook.cs
@@ -4,5 +4,86 @@ using UnityEngine;
 
 public class Rook : ChessFigure
 {
+    public override bool[,] PossibleMove()
+    {
+        bool[,] r = new bool[8, 8];
+        ChessFigure c;
+        int i;
 
+        // Left
+        i = CurrentX;
+        while(true)
+        {
+            i--;
+            if (i < 0)
+                break;
+            c = BoardManager.Instance.ChessFigurePositions[i, CurrentY];
+            if (c == null) r[i, CurrentY] = true;
+            else
+            {
+                if(c.isWhite != isWhite)
+                {
+                    r[i, CurrentY] = true;
+                }
+                break;
+            }
+        }
+
+        // Right
+        i = CurrentX;
+        while (true)
+        {
+            i++;
+            if (i >= 8)
+                break;
+            c = BoardManager.Instance.ChessFigurePositions[i, CurrentY];
+            if (c == null) r[i, CurrentY] = true;
+            else
+            {
+                if (c.isWhite != isWhite)
+                {
+                    r[i, CurrentY] = true;
+                }
+                break;
+            }
+        }
+
+        // Forward
+        i = CurrentY;
+        while (true)
+        {
+            i++;
+            if (i >= 8) break;
+            c = BoardManager.Instance.ChessFigurePositions[CurrentX, i];
+            if(c == null) r[CurrentX, i] = true;
+            else
+            {
+                if(c.isWhite != isWhite)
+                {
+                    r[CurrentX, i] = true;
+                }
+                break;
+            }
+        }
+
+        // Back
+        i = CurrentY;
+        while (true)
+        {
+            i--;
+            if (i < 0) break;
+            c = BoardManager.Instance.ChessFigurePositions[CurrentX, i];
+            if (c == null) r[CurrentX, i] = true;
+            else
+            {
+                if (c.isWhite != isWhite)
+                {
+                    r[CurrentX, i] = true;
+                }
+                break;
+            }
+        }
+
+        return r;
+    }
 }
